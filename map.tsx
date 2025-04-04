f4a750eccc335913b209a59ddd4d7d716048d28e
samuelstanley <230sjstanley@gmail.com>
2025-04-04 23:15:58 +0100

    Create map.tsx

---
+++ map.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export default function Map() {
+  return (
+    <div className="flex-1 bg-accent">
+      <p className="text-center text-white p-4">[ Map will go here ]</p>
+    </div>
+  );
+}
